e2f6f3ff707def5197814036adc0a9818cf8d5db
Lorien <LorienMCS@gmail.com>
2015-09-04 16:15:37 -0700

    add carousel functionality

---
+++ carousel/js/controllers.js
@@ -0,0 +1,16 @@
+app.controller('CarouselController', ['$scope', function($scope) {
+	$scope.slides = [
+		{img: 'images/cascade_lake.jpg', desc: 'Cascade Lake'},
+		{img: 'images/eagle_point_1.jpg', desc: 'Eagle Point'},
+		{img: 'images/eagle_point_2.jpg', desc: 'Eagle Point'},
+		{img: 'images/eagle_point_3.jpg', desc: 'Eagle Point'},
+		{img: 'images/eagle_point_4.jpg', desc: 'Eagle Point'},
+		{img: 'images/emerald_bay.jpg', desc: 'Emerald Bay'},
+		{img: 'images/ground_squirrel.jpg', desc: 'Ground Squirrel'},
+		{img: 'images/hairy_woodpecker.jpg', desc: 'Hairy Woodpecker'},
+		{img: 'images/harveys_view.jpg', desc: 'View from Harveys Lake Tahoe'},
+		{img: 'images/tallac_beach_house.jpg', desc: 'Tallac Beach House'},
+		{img: 'images/tallac_lilacs.jpg', desc: 'Tallac Lilacs'},
+		{img: 'images/vikingsholm.jpg', desc: 'Vikingsholm'},
+	];
+}])
---
+++ carousel/js/directives.js
@@ -0,0 +1,45 @@
+app.directive('lsImageCarousel', function() {
+  return {
+    restrict: 'AE',
+    scope: {
+      slides: '=',
+    },
+    templateUrl: 'partials/carousel-template.html',
+    link: function(scope, element, attrs) {
+
+      scope.currentImgIndex = 0;
+      scope.slides[scope.currentImgIndex].showing = true;
+
+      scope.prev = function() {
+        // if there are images to the left of the current image
+        if (scope.currentImgIndex > 0) {
+          scope.currentImgIndex--;
+        } else {
+          // make it look like it's an actual carousel,
+          // by going to the very far right image
+          scope.currentImgIndex = scope.images.length - 1;
+        };
+        scope.slides.forEach(function(slide){
+          slide.showing = false;
+        });
+        scope.slides[scope.currentImgIndex].showing = true;
+      };
+
+      scope.next = function() {
+        // if there are images to the right of the current image
+        if (scope.currentImgIndex < scope.slides.length - 1) {
+          scope.currentImgIndex++;
+        } else {
+          // make it look like it's an actual carousel,
+          // by going to the very far left image
+          scope.currentImgIndex = 0;
+        };
+        scope.slides.forEach(function(slide){
+          slide.showing = false;
+        });
+        scope.slides[scope.currentImgIndex].showing = true;
+      };
+
+    } // closing curly brace for link
+  }; // closing curly brace for return
+});
